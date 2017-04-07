d7a5e89b55e4058934b49fabca5d1ae21279dbee
zhaorui <zhaoruiexplorer@icloud.com>
2017-04-07 10:01:56 +0800

    Initial Commit

---
+++ CloudUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CloudUI
+//
+//  Created by 赵睿 on 07/04/2017.
+//  Copyright © 2017 com.homebrew.zhaorui. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
